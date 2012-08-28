7bacd71cccf3319597a67718a9a981bebb49de09
root <root@158ab4f7-287c-def9-a322-6dbe4328d440>
2012-08-28 19:59:07 +0000

    twig example in _tests dir

---
+++ dalite/_tests/twig/index.php
@@ -0,0 +1,13 @@
+<?php
+    require_once 'vendor/twig/twig/lib/Twig/Autoloader.php';
+    Twig_Autoloader::register();
+    $loader = new Twig_Loader_Filesystem('template');
+
+
+    $twig = new Twig_Environment($loader);
+
+    //Variable stubs go here :)
+
+    echo $twig->render('i.html', array('a_variable' => 'Koya'));
+
+?>
