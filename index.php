38cf96d3d3782960d4fd95e868dc9db889699cfd
root <root@158ab4f7-287c-def9-a322-6dbe4328d440>
2012-09-01 00:50:24 +0000

    move to templates

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     require_once 'vendor/twig/twig/lib/Twig/Autoloader.php';
     Twig_Autoloader::register();
-    $loader = new Twig_Loader_Filesystem('template');
+    $loader = new Twig_Loader_Filesystem('templates');
 
 
     $twig = new Twig_Environment($loader);
