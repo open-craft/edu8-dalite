091eb37553a042959c63576ddceb0cfb1739a626
root <root@158ab4f7-287c-def9-a322-6dbe4328d440>
2012-09-25 12:13:00 +0000

    moving the entry php page>

---
+++ www/index.php
@@ -0,0 +1,83 @@
+<?php
+ require_once 'vendor/autoload.php';
+
+
+ use Symfony\Component\HttpFoundation\Request;
+ use Symfony\Component\HttpFoundation\Response;
+ use Symfony\Component\Routing;
+ use Symfony\Component\Routing\RouteCollection;
+ use Symfony\Component\Routing\RequestContext;
+ use Symfony\Component\Routing\Matcher\UrlMatcher;
+ use Twig\Twig;
+
+
+
+
+class HelloController
+{
+    public function indexAction($name)
+    {
+      return new Response('<html><body>Hello '.$name.'!</body></html>');
+    }
+}
+/*
+class MyController
+{
+    public function indexAction()
+    {
+      // ...
+    $loader = new Twig_Loader_Filesystem('templates');
+
+
+    $twig = new Twig_Environment($loader);
+
+    //Variable stubs go here :)
+    $request = Request::createFromGlobals();
+    $response = new Response(
+    $twig->render('profs-add-question.html', 
+          array('count' => '0', 
+                'assignment' => array('title' => 'This is the title', 
+                        'concepts' => array('friction', 'matter',),),
+                'media1' => array('type' => 'jpeg',),
+                'media2' => array('type' => 'youtube',),
+                
+                //For assignment entry pages
+                'concepts' => array ('concept1', 'concept2',),
+                //Student or prof names
+                'student' => array ('firstname' => 'myfirstname', 'lastname' => 'mylastname'),
+                )
+            )); 
+	return $response;
+    }
+}
+*/
+
+
+$routes = new RouteCollection();
+//$routes->add('route_name', new Route('/foo', array('controller' => 'HelloController::indexAction')));
+$routes->add('hello', new Route('/hello/{name}', array(
+    'controller' => 'HelloController',
+)));
+
+$context = new RequestContext($_SERVER['REQUEST_URI']);
+
+$matcher = new UrlMatcher($routes, $context);
+
+$resolver = new HttpKernel\Controller\ControllerResolver();
+ 
+try {
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+ 
+    $controller = $resolver->getController($request);
+    $arguments = $resolver->getArguments($request, $controller);
+ 
+    $response = call_user_func_array($controller, $arguments);
+} catch (Routing\Exception\ResourceNotFoundException $e) {
+    $response = new Response('Not Found', 404);
+} catch (Exception $e) {
+    $response = new Response('An error occurred', 500);
+}
+ 
+$response->send();
+echo $parameters['controller'];
+?>
