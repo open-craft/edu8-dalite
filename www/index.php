96adb1758f0ad05d7d4987514e1c8231255af6a2
root <root@158ab4f7-287c-def9-a322-6dbe4328d440>
2012-09-25 12:21:08 +0000

    moving stuff aroundls

--- www/index.php
+++ www/index.php
@@ -1,14 +1,15 @@
 <?php
- require_once 'vendor/autoload.php';
-
-
+ require_once '../vendor/autoload.php';
  use Symfony\Component\HttpFoundation\Request;
  use Symfony\Component\HttpFoundation\Response;
+ use Twig\Twig;
+
+/*
+
  use Symfony\Component\Routing;
  use Symfony\Component\Routing\RouteCollection;
  use Symfony\Component\Routing\RequestContext;
  use Symfony\Component\Routing\Matcher\UrlMatcher;
- use Twig\Twig;
 
 
 
@@ -20,21 +21,21 @@ class HelloController
       return new Response('<html><body>Hello '.$name.'!</body></html>');
     }
 }
-/*
 class MyController
 {
     public function indexAction()
     {
       // ...
-    $loader = new Twig_Loader_Filesystem('templates');
+      */
+    $loader = new Twig_Loader_Filesystem('../templates');
 
 
     $twig = new Twig_Environment($loader);
 
     //Variable stubs go here :)
-    $request = Request::createFromGlobals();
-    $response = new Response(
-    $twig->render('profs-add-question.html', 
+//    $request = Request::createFromGlobals();
+//    $response = new Response(
+    echo $twig->render('profs-add-question.html', 
           array('count' => '0', 
                 'assignment' => array('title' => 'This is the title', 
                         'concepts' => array('friction', 'matter',),),
@@ -46,12 +47,11 @@ class MyController
                 //Student or prof names
                 'student' => array ('firstname' => 'myfirstname', 'lastname' => 'mylastname'),
                 )
-            )); 
+            ); 
+/*
 	return $response;
     }
 }
-*/
-
 
 $routes = new RouteCollection();
 //$routes->add('route_name', new Route('/foo', array('controller' => 'HelloController::indexAction')));
