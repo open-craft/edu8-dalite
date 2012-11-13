aec5d7b96fd7a56e2471c799f35a781fe8acd413
koya <koya@158ab4f7-287c-def9-a322-6dbe4328d440>
2012-11-13 19:34:20 +0000

    We have routing!

--- www/index.php
+++ www/index.php
@@ -1,24 +1,38 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Request;
+
+$session = new Session();
+$session->start();
+
+// set and get session attributes
+$question_num = $session->get('question_num', 0);
+$session->set('question_num', ++$question_num);
 
 $loader = new Twig_Loader_Filesystem($_SERVER['DOCUMENT_ROOT'] . '/../templates');
 $twig = new Twig_Environment($loader);
 
-echo $twig->render('question-part1.html.twig', [
-    'question_num' => '1',
-    'student' => ['firstname' => 'Dan', 'lastname' => 'Hill'],
-    'question' => [
-        [   
-            'title' => 'This is the title', 
-            'concepts' => ['friction', 'matter'],
-            'media1' => ['type' => 'jpeg'],
-            'media2' => ['type' => 'youtube']
-        ],
-        [
-            'title' => 'This is the title', 
-            'concepts' => ['friction', 'matter'],
-            'media1' => ['type' => 'jpeg'],
-            'media2' => ['type' => 'youtube']
-        ]
-        ]]);
+$request = Request::createFromGlobals();
+$path = $request->getPathInfo();
+if ($path == "/")
+    $path = "index";
+    
+    echo $twig->render(rtrim($request->getPathInfo(), "/") . '.html.twig', [
+        'question_num' => $question_num,
+        'student' => ['firstname' => 'Dan', 'lastname' => 'Hill'],
+        'question' => [
+            [
+                'title' => 'This is question',
+                'concepts' => ['friction', 'matter'],
+                'media1' => ['type' => 'jpeg'],
+                'media2' => ['type' => 'youtube']
+            ],
+            [
+                'title' => 'This is the another question',
+                'concepts' => ['friction', 'whoupie'],
+                'media1' => ['type' => 'jpeg'],
+                'media2' => ['type' => 'youtube']
+            ]
+            ]]);
 ?>
