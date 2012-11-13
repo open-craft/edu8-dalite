4dd8840c32a43741c9ddc22ed693c570a2334fee
koya <koya@158ab4f7-287c-def9-a322-6dbe4328d440>
2012-11-13 03:43:17 +0000

    sessions via symfony

--- www/index.php
+++ www/index.php
@@ -1,23 +1,21 @@
 <?php
-
 require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
-use Twig_Environment;
-
 
 $loader = new Twig_Loader_Filesystem($_SERVER['DOCUMENT_ROOT'] . '/../templates');
 $twig = new Twig_Environment($loader);
 
 echo $twig->render('question-part1.html.twig', [
-    'count' => '0',
-    'student' => ['firstname' => 'myfirstname', 'lastname' => 'mylastname'],
-    'title' => 'This is the title', 
-    'questions' => [
-        [
+    'question_num' => '1',
+    'student' => ['firstname' => 'Dan', 'lastname' => 'Hill'],
+    'question' => [
+        [   
+            'title' => 'This is the title', 
             'concepts' => ['friction', 'matter'],
             'media1' => ['type' => 'jpeg'],
             'media2' => ['type' => 'youtube']
         ],
         [
+            'title' => 'This is the title', 
             'concepts' => ['friction', 'matter'],
             'media1' => ['type' => 'jpeg'],
             'media2' => ['type' => 'youtube']
--- www/question-part1/index.php
+++ www/question-part1/index.php
@@ -1,22 +1,34 @@
 <?php
- require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
- use Twig\Twig;
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
+use Symfony\Component\HttpFoundation\Session\Session;
+use \Twig_Autoloader;
+
+$session = new Session();
+$session->start();
+
+// set and get session attributes
+$question_num = $session->get('question_num', 0);
+$session->set('question_num', ++$question_num);
+
 
     $loader = new Twig_Loader_Filesystem($_SERVER['DOCUMENT_ROOT'] . '/../templates');
     $twig = new Twig_Environment($loader);
 
-    echo $twig->render('question-part1.html.twig', 
-          array('count' => '0', 
-                'assignment' => array('title' => 'This is the title', 
-                        'concepts' => array('friction', 'matter',),),
-                'media1' => array('type' => 'jpeg',),
-                'media2' => array('type' => 'youtube',),
-                
-                //For assignment entry pages
-                'concepts' => array ('concept1', 'concept2',),
-                //Student or prof names
-                'student' => array ('firstname' => 'myfirstname', 'lastname' => 'mylastname'),
-                )
-            ); 
+    echo $twig->render('question-part1.html.twig', [
+    'question_num' => $question_num,
+    'student' => ['firstname' => 'Dan', 'lastname' => 'Hill'],
+    'question' => [
+        [   
+            'title' => 'This is question', 
+            'concepts' => ['friction', 'matter'],
+            'media1' => ['type' => 'jpeg'],
+            'media2' => ['type' => 'youtube']
+        ],
+        [
+            'title' => 'This is the another question',
+            'concepts' => ['friction', 'whoupie'],
+            'media1' => ['type' => 'jpeg'],
+            'media2' => ['type' => 'youtube']
+        ]
+        ]]);
 ?>
-
