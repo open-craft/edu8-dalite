7e56a04a01c710410f1f666b1eec7dd6c769561d
koya <koya@158ab4f7-287c-def9-a322-6dbe4328d440>
2012-11-14 00:13:40 +0000

    Next question

--- www/index.php
+++ www/index.php
@@ -7,17 +7,23 @@ $session = new Session();
 $session->start();
 
 // set and get session attributes
-$question_num = $session->get('question_num', 0);
-$session->set('question_num', ++$question_num);
+$question_num = $session->get('question_num', 1);
 
 $loader = new Twig_Loader_Filesystem($_SERVER['DOCUMENT_ROOT'] . '/../templates');
 $twig = new Twig_Environment($loader);
 
 $request = Request::createFromGlobals();
 $path = $request->getPathInfo();
+
 if ($path == "/")
     $path = "index";
-    
+
+if ($path == "/admin/")
+    $path = "/admin/index";
+
+if ($path == "/question-part4/")
+	$session->set('question_num', $question_num+1);
+
     echo $twig->render(rtrim($request->getPathInfo(), "/") . '.html.twig', [
         'question_num' => $question_num,
         'student' => ['firstname' => 'Dan', 'lastname' => 'Hill'],
