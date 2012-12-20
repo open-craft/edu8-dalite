2153ab055ae5453849b4cf3d10efcaab0f4a8f57
koya <koya@158ab4f7-287c-def9-a322-6dbe4328d440>
2012-12-20 23:15:01 +0000

    merged devel back to trunk

--- src/Edu8/Route.php
+++ src/Edu8/Route.php
@@ -17,8 +17,8 @@ class Route {
             $route_path = $entry->getRelativePath();
             $basename = $entry->getBasename('.php');
             $cleaned = str_replace('/', '_', str_replace('-', '_', $route_path . $basename));
-            $route = $route_path . ($basename === 'index') ? '' : $basename;
-
+            $route = $route_path . (($basename === 'index') ? '' :'/'. $basename);
+/*
             if (strrchr($route, '-')) {
                 $sluglen = strlen(strrchr($route, '-'));
                 $route2 = substr($route, 0, strlen($route) - $sluglen + 1) . '{slug}';
@@ -27,8 +27,11 @@ class Route {
                 $route2 = $route;
                 $basename2 = $basename;
             }
+ */
             $routes->add($cleaned, new \Symfony\Component\Routing\Route($route
-                    , ['_controller' => 'build', 'php_file' => $entry->getRealPath(), 'file_root' => $route_path . $basename, 'name' => '.+']));
+                    , ['_controller' => 'build', 'php_file' => $entry->getRealPath(), 'file_root' => $route_path.'/'.$basename, 'name' => '.+']));
+            $routes->add('slash'.$cleaned, new \Symfony\Component\Routing\Route($route.'/'
+                    , ['_controller' => 'build', 'php_file' => $entry->getRealPath(), 'file_root' => $route_path.'/'.$basename, 'name' => '.+']));
         }
 
         return $routes;
