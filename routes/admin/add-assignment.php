6254a8e78297b60faf411ba0bcdfb11760d14c01
koya <koya@158ab4f7-287c-def9-a322-6dbe4328d440>
2012-12-23 23:46:23 +0000

    choose course for add-assignment

--- routes/admin/add-assignment.php
+++ routes/admin/add-assignment.php
@@ -3,8 +3,8 @@
 function build(\Symfony\Component\HttpFoundation\Request $request, &$a) {
         $a['request']['postvar'] = '';
         $connection = \Edu8\Config::initDb();
-        $db_statement = \Edu8\Sql::runStatement($connection,'all_questions');
-        $a['all_questions'] = $db_statement->fetchAll();
+        $db_statement = \Edu8\Sql::runStatement($connection,'prof_courses', ['professor' => $a['student'][0]['student_']]);
+        $a['courses'] = $db_statement->fetchAll();
  }
 
 ?>
