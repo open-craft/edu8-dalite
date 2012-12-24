6e7fbcdaf23f4b5c2ffd1bfc66bedd8bf52ba648
koya <koya@158ab4f7-287c-def9-a322-6dbe4328d440>
2012-12-24 01:24:04 +0000

    Fixes for add-assignment

--- routes/admin/add-assignment.php
+++ routes/admin/add-assignment.php
@@ -5,6 +5,8 @@ function build(\Symfony\Component\HttpFoundation\Request $request, &$a) {
         $connection = \Edu8\Config::initDb();
         $db_statement = \Edu8\Sql::runStatement($connection,'prof_courses', ['professor' => $a['student'][0]['student_']]);
         $a['courses'] = $db_statement->fetchAll();
+        $db_statement = \Edu8\Sql::runStatement($connection,'all_questions');
+        $a['all_questions'] = $db_statement->fetchAll();
  }
 
 ?>
