fb46a3544712f79abea47dfb7d18ec815652a3c4
koya <koya@158ab4f7-287c-def9-a322-6dbe4328d440>
2013-01-03 23:09:41 +0000

    cleanup student

--- routes/index.php
+++ routes/index.php
@@ -2,7 +2,7 @@
 function build (\Symfony\Component\HttpFoundation\Request $request,&$a){
         $connection = \Edu8\Config::initDb();
         $db_statement = \Edu8\Sql::runStatement($connection,
-                'student_assignment', ['student' => $a['student'][0]['student_']]);
+                'student_assignment', ['student' => $a['student']['student_']]);
         $a['assignments'] =  $db_statement->fetchAll();
        
 
