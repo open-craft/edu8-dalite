f4317c5c7507582d78fb0d070d2125ad6c3c0766
koya <koya@158ab4f7-287c-def9-a322-6dbe4328d440>
2013-01-06 02:01:58 +0000

    worked a little magic to finish add-course...

--- callbacks/admin/add-course.php
+++ callbacks/admin/add-course.php
@@ -1,19 +1,36 @@
 <?php
-function post (\Symfony\Component\HttpFoundation\Request $request, &$a){
 
+function readLogins($path) {
+    foreach (file($path) as $row) {
+        $logins[] = explode(',', $row)[1];
+    }
+    array_shift($logins); //drop the first element
+
+    return $logins;
+}
+
+function post(\Symfony\Component\HttpFoundation\Request $request, &$a) {
     $uploaded = $request->files;
-    if($uploaded->has('file')){
+
+    if ($uploaded->has('file')) {
         #do mysql import
         $noerr = 1;
         $file = $uploaded->get('file')->getPathname();
-        system('ln -s '. $file . ' /tmp/student.csv');
+        system('ln -s ' . $file . ' /tmp/student.csv');
         system('mysqlimport --ignore-lines=1 -vv --local --fields-terminated-by="," -u root --password=xxxxxx dalite /tmp/student.csv', $noerr);
+        $params = readLogins('/tmp/student.csv');
         system('rm /tmp/student.csv');
-        if(!noerr)
+        if (!noerr || !count($params))
             throw new Edu8\Exception("import error");
-        
+
+        $connection = \Edu8\Config::initDb();
+        $connection->insert('course', ['professor_' => $a['student']['student_'], '`name`' => $a['request']['name'], '`accessible`' => '1']);
+        $course_id = $connection->lastInsertId();
+
         ## get use the where in (logins) from the file to get the student_ ids to add to student_course
+        $sql = Edu8\Sql::getStatement('insert-students-by-login');
+        $connection->executeQuery($sql, [$course_id, $params], [\PDO::PARAM_INT, \Doctrine\DBAL\Connection::PARAM_INT_ARRAY]);
     }
-            
 }
+
 ?>
