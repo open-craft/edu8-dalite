57b9cb2323f2a9d6c87716c2a3db9254036c547e
frank <frank@158ab4f7-287c-def9-a322-6dbe4328d440>
2013-01-28 16:02:57 +0000

    puts proper file extensions on images (jpegs, gifs, pngs), leaves everything else alone.

--- www/media-upload.ajax.php
+++ www/media-upload.ajax.php
@@ -5,7 +5,29 @@
 
 $tmp_name = $_FILES['media1']['tmp_name'];
 
-$image_name = $_FILES['media1']['name'];
+$info = finfo_open(FILEINFO_MIME_TYPE);
+$mime_type = finfo_file($info, $tmp_name);
+finfo_close($info);
+
+$extension = '';
+switch($mime_type) {
+    case 'image/jpeg':
+        $extension = 'jpg';
+        break;
+    
+    case 'image/gif':
+        $extension = 'gif';
+        break;
+    
+    case 'image/png':
+        $extension = 'png';
+        break; 
+    
+    default:
+        $extension = pathinfo($_FILES['media1']['name'])['extension'];
+}
+
+$image_name = pathinfo($_FILES['media1']['name'])['filename'] . '.' . $extension;
 $image_file = 'img-uploads/' . $image_name;
    
 try {
