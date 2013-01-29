5ac346f5e54d8c786db4703c0a16b3ef5d850fec
koya <koya@158ab4f7-287c-def9-a322-6dbe4328d440>
2013-01-29 00:21:39 +0000

    sort by lastname

--- sql/results.sql
+++ sql/results.sql
@@ -3,4 +3,4 @@ join student on student.student_ = response.student_
 join assignment_question on assignment_question.assignment_ = response.assignment_ and assignment_question.question_ = response.question_
 join question on question.question_ = response.question_
 WHERE response.assignment_ = :assignment 
-ORDER BY response.student_, assignment_question.`order`, response.attempt
+ORDER BY student.lastname, response.student_, assignment_question.`order`, response.attempt
