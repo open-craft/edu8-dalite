8863a72fd83fdaa36f2ac2141ab211e294937261
frank <frank@158ab4f7-287c-def9-a322-6dbe4328d440>
2013-02-01 04:37:22 +0000

    gets a youtube id from whatever string the user inputs, handles switching between youtube video and image upload with more grace.

--- www/scripts/media-upload.js
+++ www/scripts/media-upload.js
@@ -14,27 +14,24 @@ $(document).ready(
                     function(data) {
                         switch(data.target.value) {
                             case 'Video':
-                                $('#collapseOne #img_success').hide();
-                                $('#collapseOne #img_show').hide();
-                                
-                                $('#collapseOne #youframe').show();
+                                $('#collapseOne #img_div').hide();                                
+                                $('#collapseOne #youdiv').show();
                                 break;
                                 
                             case 'Image':
-                                $('#collapseOne #img_success').show();
-                                $('#collapseOne #img_show').show();
-                                
-                                $('#collapseOne #youframe').hide();
+                                $('#collapseOne #img_div').show();                               
+                                $('#collapseOne #youdiv').hide();
                                 break;
                         }
                     }
                 );
                 
             $('#collapseOne #youtube').change(
-                    function(data) {
-                        var html = '<iframe id="youframe" width="420" height="345" src="http://youtube.com/embed/' + getVar('v', data.target.value) + '"></iframe>';
+                    function(data) {                        
+                        var id = getYoutubeId(data.target.value);
                         
-                        $('#collapseOne #youtube').after(html);
+                        $('#collapseOne #youframe').attr('src', 'http://youtube.com/embed/' + id);
+                        $('#collapseOne #youframe').show();
                     }
                 );
     
@@ -44,21 +41,6 @@ $(document).ready(
             $('#collapseOne input[type=file]').change(
                     function(filedata) {
                         $('#image').submit();
-                
-                        /*  Did not work, the $_FILES array in the php file was empty.
-                        
-                        var filename = $('#collapseOne input[type=file]').val();
-                        
-                        alert(filedata.target.value);
-                        
-                        $.ajax(
-                                {type: "POST",
-                                 url: "/media-upload.ajax.php",
-                                 enctype: "multipart/form-data",
-                                 data: {file: filename},
-                                 success: function(data) {alert(data);},
-                                 error: function(data) {alert(data.responseText);}}
-                        );*/
                     }
             );
         }
@@ -74,58 +56,23 @@ function uploadReady(file) {
 
     $('#collapseOne #filename').val(file);
     
-    $('#collapseOne #img_success').text(file + ' uploaded');
-    
     if(ext != 'jpg' && ext != 'png' && ext != 'gif') {
+        $('#collapseOne #img_success').text('please choose an image file (jpeg, gif, or png)');
         return;
+    } else {
+        $('#collapseOne #img_success').text(file + ' uploaded');
     }
      
-    if($('#collapseOne #img_show').length === 0) {
-        var html = '<img id="img_show" style="display:block;" src="' + file_url + '" />';    
-        $('#collapseOne #img_success').before(html); 
-    } else {
-        $('#collapseOne #img_show').attr('src', file_url);
-    }
+    $('#collapseOne #img_show').attr('src', file_url);
+    $('#collapseOne #img_show').show();
 }
 
 /*********************************************************
  Used to properly display a youtube video, by getting its
- id from the query string.
+ 11 character id.
 
  *********************************************************/
-function getVar(name, get_string)
-         {
-         return_value = '';
-         
-         do { //This loop is made to catch all instances of any get variable.
-            name_index = get_string.indexOf(name + '=');
-            
-            if(name_index != -1)
-              {
-              get_string = get_string.substr(name_index + name.length + 1, get_string.length - name_index);
-              
-              end_of_value = get_string.indexOf('&');
-              if(end_of_value != -1)                
-                value = get_string.substr(0, end_of_value);                
-              else                
-                value = get_string;                
-                
-              if(return_value == '' || value == '')
-                 return_value += value;
-              else
-                 return_value += ', ' + value;
-              }
-            } while(name_index != -1)
-            
-         //Restores all the blank spaces.
-         space = return_value.indexOf('+');
-         while(space != -1)
-              { 
-              return_value = return_value.substr(0, space) + ' ' + 
-              return_value.substr(space + 1, return_value.length);
-							 
-              space = return_value.indexOf('+');
-              }
-          
-         return(return_value);        
-         }
+
+function getYoutubeId(text) {
+    return(text.match(/[a-zA-Z0-9\-\_]{11}/));
+}
