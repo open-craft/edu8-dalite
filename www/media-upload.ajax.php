6e1214f1b3e7f1683af6dba27627747bed3422e8
frank <frank@158ab4f7-287c-def9-a322-6dbe4328d440>
2013-02-03 05:33:09 +0000

    Now handles 2 images.

--- www/media-upload.ajax.php
+++ www/media-upload.ajax.php
@@ -3,42 +3,54 @@
 //If the location is changed, so should $image_file,
 //since the path is relative to this script.
 
-$tmp_name = $_FILES['media1']['tmp_name'];
-
-$info = finfo_open(FILEINFO_MIME_TYPE);
-$mime_type = finfo_file($info, $tmp_name);
-finfo_close($info);
-
-$extension = '';
-switch($mime_type) {
-    case 'image/jpeg':
-        $extension = 'jpg';
-        break;
+function getUploadedFile($name) {
+    $tmp_name = $_FILES[$name]['tmp_name'];
     
-    case 'image/gif':
-        $extension = 'gif';
-        break;
+    $info = finfo_open(FILEINFO_MIME_TYPE);
+    $mime_type = finfo_file($info, $tmp_name);
+    finfo_close($info);
     
-    case 'image/png':
-        $extension = 'png';
-        break; 
+    $extension = '';
+    switch($mime_type) {
+        case 'image/jpeg':
+            $extension = 'jpg';
+            break;
     
-    default:
-        $extension = '';
+        case 'image/gif':
+            $extension = 'gif';
+            break;
+    
+        case 'image/png':
+            $extension = 'png';
+            break; 
+    
+        default:
+            $extension = '';
+    }
+    
+    if($extension != '') {
+        $image_name = pathinfo($_FILES[$name]['name'])['filename'] . '.' . $extension;
+    
+        $image_file = 'img-uploads/' . $image_name;
+    
+        try {
+            move_uploaded_file($tmp_name, $image_file);
+        }
+        catch (Exception $e) {
+            print_r($e);    
+        }
+    }
+    
+    return($image_name);
 }
 
-if($extension != '') {
-    $image_name = pathinfo($_FILES['media1']['name'])['filename'] . '.' . $extension;
-    
-    $image_file = 'img-uploads/' . $image_name;
-    
-    try {
-        move_uploaded_file($tmp_name, $image_file);
-    }
-    catch (Exception $e) {
-        print_r($e);    
-    }
+if(isset($_FILES['media1'])) {
+    $file_name = getUploadedFile('media1');
+    echo("uploadReady('" . $file_name ."');");
+}
 
-    echo($image_name);
+if(isset($_FILES['media2'])) {
+    $file_name = getUploadedFile('media2');
+    echo("uploadReadyB('" . $file_name ."');");
 }
 ?>
