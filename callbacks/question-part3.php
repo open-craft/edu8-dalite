2461096633d785990c0ee88188479c6eb9a7a53b
koya <koya@158ab4f7-287c-def9-a322-6dbe4328d440>
2013-02-12 00:35:24 +0000

    don't record prof in student view

--- callbacks/question-part3.php
+++ callbacks/question-part3.php
@@ -17,16 +17,17 @@ function post(&$a) {
         $a['message_dlg'] = 'Please complete this question first.';
         \Edu8\Http::Redirect('/question-part3', $a);
     }
-    
-    if(array_key_exists('question_num', $a)) {
-       $concepts = implode(",", preg_grep_keys('/^tag/', $a['request']));
-        $connection = \Edu8\Config::initDb();
-        try {
-        $connection->insert('response', ['student_' => $a['student']['student_'], 'assignment_' => $a['assignment'], 'question_' => $a['question'][$a['question_num']]['question_'], 'attempt' => '0', 'answer' => $a['request']['answer'], 'rationale' => $a['request']['rationale'], 'concepts' => $concepts]);
-        $connection->insert('response', ['student_' => $a['student']['student_'], 'assignment_' => $a['assignment'], 'question_' => $a['question'][$a['question_num']]['question_'], 'attempt' => '1', 'answer' => implode(',', preg_grep_keys('/^C/', $a['request'])), 'rationale' => $a['request']['rationale'], 'concepts' => $concepts]);
-        }catch(Exception $e){
-            echo 'RESUBMIT ignored.';
-        }
+    if(!$a['student']['is_professor']){    
+	    if(array_key_exists('question_num', $a)) {
+	       $concepts = implode(",", preg_grep_keys('/^tag/', $a['request']));
+		$connection = \Edu8\Config::initDb();
+		try {
+		$connection->insert('response', ['student_' => $a['student']['student_'], 'assignment_' => $a['assignment'], 'question_' => $a['question'][$a['question_num']]['question_'], 'attempt' => '0', 'answer' => $a['request']['answer'], 'rationale' => $a['request']['rationale'], 'concepts' => $concepts]);
+		$connection->insert('response', ['student_' => $a['student']['student_'], 'assignment_' => $a['assignment'], 'question_' => $a['question'][$a['question_num']]['question_'], 'attempt' => '1', 'answer' => implode(',', preg_grep_keys('/^C/', $a['request'])), 'rationale' => $a['request']['rationale'], 'concepts' => $concepts]);
+		}catch(Exception $e){
+		    echo 'RESUBMIT ignored.';
+		}
+		}
     }
 }
 ?>
