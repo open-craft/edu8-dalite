3f1dbfd40f2df3bfa749c3503ca22b506cfe8ae7
koya <koya@158ab4f7-287c-def9-a322-6dbe4328d440>
2013-02-16 23:26:50 +0000

    Allow students to leave on page1

--- callbacks/question-part1.php
+++ callbacks/question-part1.php
@@ -2,10 +2,6 @@
 
 
 function post(&$a) {
-    if($a['request']['pathname'] != '/question-part2' && $a['request']['pathname'] != '/question-part1'){
-        $a['message_dlg'] = 'Please complete this question first.';
-        \Edu8\Http::Redirect('/question-part1', $a);
-    }
 
 }
 ?>
