4cfe842ea4da1802633ae98bc345a4375186a1e3
koya <koya@158ab4f7-287c-def9-a322-6dbe4328d440>
2013-02-16 22:50:54 +0000

    Bug #89: Counting reponses from student to set where they are in the assignment if the session was terminated

--- routes/question-part1.php
+++ routes/question-part1.php
@@ -1,18 +1,32 @@
 <?php
 
 function build(&$a) {
-    $a['alpha'] = ['Z','A','B','C','D','E','F','G','H','I'];
+    if (!array_key_exists('assignment', $a['request'])) {
+        unset($a['question_num']);
+        \Edu8\Http::redirect('/');
+    }
 
-    if(!array_key_exists('question_num', $a) || ($a['assignment'] != $a['request']['assignment'])) {
-        $a['assignment'] = $a['request']['assignment'];
-        $connection = \Edu8\Config::initDb();
-        $db_statement = \Edu8\Sql::runStatement($connection, 'assignment_question', ['assignment' => $a['assignment']]);
-        $a['question'] = $db_statement->fetchAll();
+    $a['alpha'] = ['Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+    $a['assignment'] = $a['request']['assignment'];
+    $connection = \Edu8\Config::initDb();
+
+    if (!array_key_exists('question_num', $a)) {
+        $completed_statement = \Edu8\Sql::runStatement($connection, 'completed', ['student' => $a['student']['student_'], 'assignment' => $a['assignment']]);
+        $q_completed = $completed_statement->fetchAll();
+
+        $question_statement = \Edu8\Sql::runStatement($connection, 'assignment_question', ['assignment' => $a['assignment']]);
+        $a['question'] = $question_statement->fetchAll();
 
         foreach ($a['question'] as &$q) {
             $q['concepts'] = preg_split('/,/', $q['concepts']);
         }
-        $a['question_num'] = 0;
+
+        $a['question_num'] = ($q_completed[0]['q_completed'] >> 1) - 1; // div 2 minus 1
+
+        if (count($a['question']) <= $a['question_num']) {
+            unset($a['question_num']);
+            \Edu8\Http::redirect('/');
+        }
     }
 }
 
