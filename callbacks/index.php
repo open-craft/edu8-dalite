9ffc1ec67b3a3304a00a62bc1ff3c0920c7714a5
koya <koya@158ab4f7-287c-def9-a322-6dbe4328d440>
2013-02-18 07:24:31 +0000

    This must go in the callback for index not the route.

---
+++ callbacks/index.php
@@ -0,0 +1,7 @@
+<?php
+
+function build(&$a) {
+    unset($a['message_dlg']);
+}
+
+?>
--- routes/index.php
+++ routes/index.php
@@ -1,7 +1,6 @@
 <?php
 
 function build(&$a) {
-    unset($a['message_dlg']);
     $connection = \Edu8\Config::initDb();
     $db_statement = \Edu8\Sql::runStatement($connection, 'student_assignment', ['student_' => $a['student']['student_']]);
     $a['assignments'] = $db_statement->fetchAll();
