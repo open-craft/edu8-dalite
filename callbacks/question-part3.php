ac53065a57b612b3788877a67df2ec051eb6b304
koya <koya@158ab4f7-287c-def9-a322-6dbe4328d440>
2013-02-19 21:33:48 +0000

    Default test values for part3

--- callbacks/question-part3.php
+++ callbacks/question-part3.php
@@ -1,33 +1,37 @@
 <?php
 
-function preg_grep_keys( $pattern, $input, $flags = 0 )
-{
-    $keys = preg_grep( $pattern, array_keys( $input ), $flags );
+function preg_grep_keys($pattern, $input, $flags = 0) {
+    $keys = preg_grep($pattern, array_keys($input), $flags);
     $vals = array();
-    $i=0;
-    foreach ( $keys as $key )
-    {
+    $i = 0;
+    foreach ($keys as $key) {
         $vals[$i++] = $input[$key];
     }
     return $vals;
 }
 
 function post(&$a) {
-    if($a['request']['pathname'] != '/question-part4' && $a['request']['pathname'] != '/question-part3'){
+    if ($a['request']['pathname'] != '/question-part4' && $a['request']['pathname'] != '/question-part3') {
         $a['message_dlg'] = 'Please complete this question first.';
         \Edu8\Http::Redirect('/question-part3', $a);
     }
-    if(!$a['student']['is_professor']){    
-	    if(array_key_exists('question_num', $a)) {
-	       $concepts = implode(",", preg_grep_keys('/^tag/', $a['request']));
-		$connection = \Edu8\Config::initDb();
-		try {
-		$connection->insert('response', ['student_' => $a['student']['student_'], 'assignment_' => $a['assignment'], 'question_' => $a['question'][$a['question_num']]['question_'], 'attempt' => '0', 'answer' => $a['request']['answer'], 'rationale' => $a['request']['rationale'], 'concepts' => $concepts]);
-		$connection->insert('response', ['student_' => $a['student']['student_'], 'assignment_' => $a['assignment'], 'question_' => $a['question'][$a['question_num']]['question_'], 'attempt' => '1', 'answer' => implode(',', preg_grep_keys('/^C/', $a['request'])), 'rationale' => $a['request']['rationale'], 'concepts' => $concepts]);
-		}catch(Exception $e){
-		    echo 'RESUBMIT ignored.';
-		}
-		}
+    if (!$a['student']['is_professor']) {
+        if (array_key_exists('question_num', $a)) {
+            $concepts = implode(",", preg_grep_keys('/^tag/', $a['request']));
+            $connection = \Edu8\Config::initDb();
+            //Testing.
+            $a['request']['second_answer'] = preg_grep_keys('/^C/', $a['request'])[0];
+            $a['answered_correct'][0] = 0;
+            $a['answered_correct'][0] = 1;
+            try {
+                $connection->insert('response', ['student_' => $a['student']['student_'], 'assignment_' => $a['assignment'], 'question_' => $a['question'][$a['question_num']]['question_'], 'attempt' => '0', 'answer' => $a['request']['answer'], 'rationale' => $a['request']['rationale'], 'concepts' => $concepts]);
+                $connection->insert('response', ['student_' => $a['student']['student_'], 'assignment_' => $a['assignment'], 'question_' => $a['question'][$a['question_num']]['question_'], 'attempt' => '1', 'answer' => $a['request']['second_answer'], 'rationale' => $a['request']['rationale'], 'concepts' => $concepts]);
+            } catch (Exception $e) {
+                unset($e);
+                //echo 'RESUBMIT ignored.';
+            }
+        }
     }
 }
+
 ?>
