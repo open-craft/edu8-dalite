7d9d994bef53f45ae0ae86113f44d9d6bccf79f6
koya <koya@158ab4f7-287c-def9-a322-6dbe4328d440>
2013-02-21 02:04:22 +0000

    Clear the request of Cn post vars

--- routes/question-part3.php
+++ routes/question-part3.php
@@ -3,6 +3,11 @@
 function build(&$a) {
     $connection = \Edu8\Config::initDb();
     $q = $a['question'][$a['question_num']];
+    unset($a['request']['C1']);
+    unset($a['request']['C2']);
+    unset($a['request']['C3']);
+    unset($a['request']['C4']);
+    unset($a['request']['C5']);
 
     if($a['request']['answer'] == $q['answer']) 
         $second_choice = $q['second_choice'];
