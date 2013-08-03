acf4dd32a5d8bdd9bcf745c2348cdfa405e72e29
root <root@158ab4f7-287c-def9-a322-6dbe4328d440>
2013-08-03 04:23:22 +0000

    use published flag for available assignments

--- sql/student_assignment.sql
+++ sql/student_assignment.sql
@@ -3,4 +3,4 @@ FROM student
 NATURAL JOIN student_course
 NATURAL JOIN course_assignment
 NATURAL JOIN assignment
-WHERE student_ = :student_
+WHERE student_ = :student_ AND assignment.published = 1
